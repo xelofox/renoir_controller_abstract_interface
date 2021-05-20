85ff81a21427257a7f6d5739f1f709284e1625c9
xelofox <38127459+xelofox@users.noreply.github.com>
2021-05-20 17:49:30 +0200

    ee

--- include/rtwtypes.h
+++ include/rtwtypes.h
@@ -7,9 +7,9 @@
 //
 // Code generated for Simulink model 'walk'.
 //
-// Model version                  : 1.261
+// Model version                  : 1.263
 // Simulink Coder version         : 9.1 (R2019a) 23-Nov-2018
-// C/C++ source code generated on : Thu May 20 17:43:49 2021
+// C/C++ source code generated on : Thu May 20 17:48:55 2021
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: Intel->x86-64 (Windows64)
