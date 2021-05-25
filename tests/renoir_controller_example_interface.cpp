1204a23999df6744efcda6c5d25c0859a78837da
xelofox <38127459+xelofox@users.noreply.github.com>
2021-05-25 15:20:05 +0200

    s

--- tests/renoir_controller_example_interface.cpp
+++ tests/renoir_controller_example_interface.cpp
@@ -154,10 +154,10 @@ public:
 
     for (size_t i=0; i<control_.size(); i++) control_[i]= torque[i];
     
-    for (size_t i=0; i<control_.size(); i++) {
+    /*for (size_t i=0; i<control_.size(); i++) {
 		std::cout<< "Torque " <<i<<" = "<< torque[i]<<std::endl;
-	}
-	std::cout << std::endl << std::endl<<std::endl;
+	}*/
+	std::cout << std::endl << std::endl;
 
     controlOut["control"].setValues(control_);
   }
