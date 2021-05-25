8e24cddd57c291d9560f29b2ce32630c80e7733b
xelofox <38127459+xelofox@users.noreply.github.com>
2021-05-25 19:44:11 +0200

    q

--- tests/ert_main.cpp
+++ tests/ert_main.cpp
@@ -7,9 +7,9 @@
 //
 // Code generated for Simulink model 'walk'.
 //
-// Model version                  : 1.63
+// Model version                  : 1.65
 // Simulink Coder version         : 9.1 (R2019a) 23-Nov-2018
-// C/C++ source code generated on : Tue May 25 19:40:15 2021
+// C/C++ source code generated on : Tue May 25 19:43:37 2021
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: Intel->x86-64 (Windows64)
