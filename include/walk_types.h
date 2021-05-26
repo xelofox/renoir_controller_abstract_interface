55d4c52350699e1c4451967507e59b2cc39ab2f5
xelofox <38127459+xelofox@users.noreply.github.com>
2021-05-26 16:00:31 +0200

    m

--- include/walk_types.h
+++ include/walk_types.h
@@ -9,7 +9,7 @@
 //
 // Model version                  : 1.266
 // Simulink Coder version         : 9.1 (R2019a) 23-Nov-2018
-// C/C++ source code generated on : Wed May 26 15:21:53 2021
+// C/C++ source code generated on : Wed May 26 15:59:39 2021
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: Intel->x86-64 (Windows64)
@@ -135,10 +135,19 @@ typedef struct {
 #define typedef_b_struct_T_walk_T
 
 typedef struct {
-  char_T Value[18];
+  char_T Value[19];
 } b_struct_T_walk_T;
 
 #endif                                 //typedef_b_struct_T_walk_T
+
+#ifndef typedef_c_struct_T_walk_T
+#define typedef_c_struct_T_walk_T
+
+typedef struct {
+  char_T Value[18];
+} c_struct_T_walk_T;
+
+#endif                                 //typedef_c_struct_T_walk_T
 
 // Custom Type definition for MATLAB Function: '<Root>/gait_update'
 #ifndef struct_tag_sJCxfmxS8gBOONUZjbjUd9E
