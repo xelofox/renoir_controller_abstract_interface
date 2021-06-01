658bd149a1b0c22c8f3d7f8b50c9a78efdc3515c
xelofox <38127459+xelofox@users.noreply.github.com>
2021-06-01 20:23:06 +0200

    q

--- tests/ert_main.cpp
+++ tests/ert_main.cpp
@@ -7,9 +7,9 @@
 //
 // Code generated for Simulink model 'walk'.
 //
-// Model version                  : 1.269
+// Model version                  : 1.270
 // Simulink Coder version         : 9.1 (R2019a) 23-Nov-2018
-// C/C++ source code generated on : Tue Jun  1 19:32:49 2021
+// C/C++ source code generated on : Tue Jun  1 20:21:17 2021
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: Intel->x86-64 (Windows64)
--- tests/walk.cpp
+++ tests/walk.cpp
@@ -7,9 +7,9 @@
 //
 // Code generated for Simulink model 'walk'.
 //
-// Model version                  : 1.269
+// Model version                  : 1.270
 // Simulink Coder version         : 9.1 (R2019a) 23-Nov-2018
-// C/C++ source code generated on : Tue Jun  1 19:32:49 2021
+// C/C++ source code generated on : Tue Jun  1 20:21:17 2021
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: Intel->x86-64 (Windows64)
@@ -16925,8 +16925,8 @@ namespace renoir_controller
         walk_DW.robot_init = 0.0;
       }
 
-      // '<S3>:1:14' if Initial_test(q,qp)
-      if (walk_Initial_test(q, Tau)) {
+      // '<S3>:1:14' if Initial_test(q,qp)||(t>30)
+      if (walk_Initial_test(q, Tau) || (walk_DW.t > 30.0)) {
         // '<S3>:1:15' pos_init=0;
         walk_DW.pos_init = 0.0;
 
