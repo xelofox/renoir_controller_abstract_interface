2df96c76442cead3651d621c6c05d26ef8957814
xelofox <38127459+xelofox@users.noreply.github.com>
2021-06-03 14:33:29 +0200

    s

--- tests/walk_data.cpp
+++ tests/walk_data.cpp
@@ -9,7 +9,7 @@
 //
 // Model version                  : 1.273
 // Simulink Coder version         : 9.1 (R2019a) 23-Nov-2018
-// C/C++ source code generated on : Thu Jun  3 14:28:03 2021
+// C/C++ source code generated on : Thu Jun  3 14:32:55 2021
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: Intel->x86-64 (Windows64)
@@ -255,13 +255,13 @@ const ConstP_walk_T walk_ConstP = {
   //  Referenced by: '<Root>/Data Store Memory52'
 
   { 10000.0, 5000.0, 5000.0, 5000.0, 10000.0, 10000.0, 10000.0, 5000.0, 5000.0,
-    5000.0, 80.0, 80.0, 20.0, 20.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0,
+    5000.0, 200.0, 200.0, 20.0, 20.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0,
     10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0 },
 
   // Expression: Kv
   //  Referenced by: '<Root>/Data Store Memory53'
 
-  { 100.0, 50.0, 50.0, 50.0, 100.0, 100.0, 100.0, 50.0, 50.0, 50.0, 0.8, 0.8,
+  { 100.0, 50.0, 50.0, 50.0, 100.0, 100.0, 100.0, 50.0, 50.0, 50.0, 2.0, 2.0,
     0.2, 0.2, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
     0.1 },
 
