2a9949b62f8a63d913ce254bebbcf464d002e93c
xelofox <38127459+xelofox@users.noreply.github.com>
2021-06-03 13:17:01 +0200

    z

--- tests/walk.cpp
+++ tests/walk.cpp
@@ -9,7 +9,7 @@
 //
 // Model version                  : 1.273
 // Simulink Coder version         : 9.1 (R2019a) 23-Nov-2018
-// C/C++ source code generated on : Wed Jun  2 19:12:05 2021
+// C/C++ source code generated on : Thu Jun  3 13:15:58 2021
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: Intel->x86-64 (Windows64)
@@ -14785,9 +14785,9 @@ namespace renoir_controller
     walk_TALOS_Newton_Euler_xelo(walk_B.T_l, qp, qpp, CoM, M1, Tau);
 
     // 'Time_ZMP_control:37' ~
-    // 'Time_ZMP_control:39' k=4;
+    // 'Time_ZMP_control:39' k=5;
     // 'Time_ZMP_control:40' fprintf("error %f = %f \n",k,hd(k)-h(k))
-    printf("error %f = %f \n", 4.0, hd[3] - h[3]);
+    printf("error %f = %f \n", 5.0, hd[4] - h[4]);
     fflush(stdout);
   }
 
