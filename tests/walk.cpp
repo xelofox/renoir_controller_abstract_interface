fa9ec4782c6821230b0741742a639cdf19d7b35f
xelofox <38127459+xelofox@users.noreply.github.com>
2021-06-09 16:27:03 +0200

    f

--- tests/walk.cpp
+++ tests/walk.cpp
@@ -9,7 +9,7 @@
 //
 // Model version                  : 1.293
 // Simulink Coder version         : 9.1 (R2019a) 23-Nov-2018
-// C/C++ source code generated on : Wed Jun  9 16:13:05 2021
+// C/C++ source code generated on : Wed Jun  9 16:26:29 2021
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: Intel->x86-64 (Windows64)
@@ -14802,9 +14802,9 @@ namespace renoir_controller
 
     // 'Time_ZMP_control:44' ~
     // fprintf("Tau correct %f = %f \n",k,Tau(k));
-    // 'Time_ZMP_control:47' k=15;
+    // 'Time_ZMP_control:47' k=16;
     // 'Time_ZMP_control:48' fprintf("error %f = %f \n",k,hd(k)-h(k))
-    printf("error %f = %f \n", 15.0, hd[14] - h[14]);
+    printf("error %f = %f \n", 16.0, hd[15] - h[15]);
     fflush(stdout);
 
     //  fprintf("hdpp %f = %f \n",k,hdpp(k))
