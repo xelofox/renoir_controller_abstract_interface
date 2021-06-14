32a9bb10029f68a9e3f59b8183ffd80fd9f6e63d
xelofox <38127459+xelofox@users.noreply.github.com>
2021-06-14 16:05:28 +0200

    s

---
+++ tests/origin_frame.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python  
+import rospy
+
+# Because of transformations
+import tf_conversions
+
+import tf2_ros
+import geometry_msgs.msg
+import dynamic_graph_bridge_msgs.msg
+
+global my_data
+
+def callback(data):
+	my_data=data.data
+	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+	
+
+def get_floating_base():
+	rospy.init_node('listener_floating_base', anonymous=True)
+	rospy.Subscriber("/simulator/floating_base_state", String, callback)
+
+
+if __name__ == '__main__':
+    rospy.init_node('transform_broadcaster')
+    rospy.Subscriber('/simu/base_odom',
+                     dynamic_graph_bridge_msgs.msg.Vector,
+                     handle_odom)
+    rospy.spin()
+
