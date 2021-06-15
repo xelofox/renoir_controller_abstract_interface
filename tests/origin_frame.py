fc19eebf105cf47ca4291d6c0877f52381e9d2ea
xelofox <38127459+xelofox@users.noreply.github.com>
2021-06-15 12:16:19 +0200

    Update origin_frame.py

--- tests/origin_frame.py
+++ tests/origin_frame.py
@@ -27,11 +27,29 @@ def get_floating_base():
 		rospy.Subscriber("/simulator/floating_base_state", Odometry, callback)
 		global my_data
 		rospy.loginfo(my_data)
+		
+def send floating_base(msg):
+	br = tf2_ros.TransformBroadcaster()
+    t = geometry_msgs.msg.TransformStamped()
+
+    t.header.stamp = rospy.Time.now()
+    t.header.frame_id = "odom"
+    t.child_frame_id = "base_link"
+    t.transform.translation.x = msg.pose.pose.position.x
+    t.transform.translation.y = msg.pose.pose.position.y
+    t.transform.translation.z = msg.pose.pose.position.z
+    t.transform.rotation.x = msg.pose.pose.orientation.x
+    t.transform.rotation.y = msg.pose.pose.orientation.y
+    t.transform.rotation.z = msg.pose.pose.orientation.z
+    t.transform.rotation.w = msg.pose.pose.orientation.w
+
+    br.sendTransform(t)
+	
 
 
 if __name__ == '__main__':
 	rospy.init_node('listener_floating_base', anonymous=True)
-	rospy.Subscriber("/simulator/floating_base_state", Odometry, callback)
+	rospy.Subscriber("/simulator/floating_base_state", Odometry, floating_base)
 	rospy.spin()
 	#get_floating_base()
 
