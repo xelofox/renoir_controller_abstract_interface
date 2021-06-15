7c73ac82c42878a2606517cc6ba6d154e0dcdc91
xelofox <38127459+xelofox@users.noreply.github.com>
2021-06-15 12:25:25 +0200

    Update origin_frame.py

--- tests/origin_frame.py
+++ tests/origin_frame.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python  
 import rospy
 
-# Because of transformations
-#import tf_conversions
-#from std_msgs.msg import String
+import tf2_ros
 from nav_msgs.msg import Odometry
-#import tf2_ros
 import geometry_msgs.msg
-#import dynamic_graph_bridge_msgs.msg
 
 global my_data
 my_data=0;
@@ -30,7 +26,18 @@ def get_floating_base():
 		
 def floating_base(msg):
 	br = tf2_ros.TransformBroadcaster()
-
+    t = geometry_msgs.msg.TransformStamped()
+    t.header.stamp = rospy.Time.now()
+    t.header.frame_id = "odom"
+    t.child_frame_id = "base_link"
+    t.transform.translation.x = msg.pose.pose.position.x
+    t.transform.translation.y = msg.pose.pose.position.y
+    t.transform.translation.z = msg.pose.pose.position.z
+    t.transform.rotation.x = msg.pose.pose.orientation.x
+    t.transform.rotation.y = msg.pose.pose.orientation.y
+    t.transform.rotation.z = msg.pose.pose.orientation.z
+    t.transform.rotation.w = msg.pose.pose.orientation.w
+    br.sendTransform(t)
 	
 
 
