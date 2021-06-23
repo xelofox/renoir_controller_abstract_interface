d991a574b8cbe7d1f56709c4cf05570d13bd2cda
xelofox <38127459+xelofox@users.noreply.github.com>
2021-06-23 14:33:00 +0200

    Update renoir_controller_example_interface.cpp

--- tests/renoir_controller_example_interface.cpp
+++ tests/renoir_controller_example_interface.cpp
@@ -157,6 +157,7 @@ public:
     /*for (size_t i=0; i<control_.size(); i++) {
 		std::cout<< "Torque " <<i<<" = "<< torque[i]<<std::endl;
 	}*/
+	std::cout <<ros::Time::now() << std::endl;
 	std::cout << std::endl;
 
     controlOut["control"].setValues(control_);
